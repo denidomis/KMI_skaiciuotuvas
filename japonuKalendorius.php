3c3703eaecdd95c37661df62b1541ede6582ca32
Domantas Denisovas <dodenisas@gmail.com>
2023-06-08 23:41:18 +0300

    pataisytas kalendorius

--- japonuKalendorius.php
+++ japonuKalendorius.php
@@ -62,77 +62,25 @@
 $metai = isset($_POST['metai']) ? $_POST['metai'] : null;
 
 function nustatytiSpalvaPagalKalendoriu($metai) {
-    $cikloIlga = 60; // Ciklo ilgis
+    $sk = ($metai-4)%60+1;
 
-    // Apskaičiuojame numerį metų cikle
-    $numerisCikle = ($metai - 1983) % $cikloIlga;
+    $numerisCikle = ($sk+10)%10;
 
-    // Nustatome spalvą pagal numerį cikle
     switch ($numerisCikle) {
         case 1:
         case 2:
-        case 11:
-        case 12:
-        case 21:
-        case 22:
-        case 31:
-        case 32:
-        case 41:
-        case 42:
-        case 51:
-        case 52:
             return "ŽALIA";
         case 3:
         case 4:
-        case 13:
-        case 14:
-        case 23:
-        case 24:
-        case 33:
-        case 34:
-        case 43:
-        case 44:
-        case 53:
-        case 54:
             return "RAUDONA";
         case 5:
         case 6:
-        case 15:
-        case 16:
-        case 25:
-        case 26:
-        case 35:
-        case 36:
-        case 45:
-        case 46:
-        case 55:
-        case 56:
             return "GELTONA";
         case 7:
         case 8:
-        case 17:
-        case 18:
-        case 27:
-        case 28:
-        case 37:
-        case 38:
-        case 47:
-        case 48:
-        case 57:
-        case 58:
             return "BALTA";
         case 9:
         case 10:
-        case 19:
-        case 20:
-        case 29:
-        case 30:
-        case 39:
-        case 40:
-        case 49:
-        case 50:
-        case 59:
-        case 0:
             return "JUODA";
         default:
             return "Nėra duomenų";
